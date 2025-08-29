94f77e47c418c0fe0ddce3f77555fbac33fb0428
Rajat Mantri <115399183+RajatMantri@users.noreply.github.com>
2025-08-29 11:36:06 +0530

    Time: 16 ms (16.57%), Space: 21.9 MB (100%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -0,0 +1,35 @@
+static auto speedup = []()  {
+    ios_base::sync_with_stdio(false);
+    cout.tie(NULL);
+    cin.tie(NULL);
+    return NULL;
+}();
+class Solution {
+    bool solve(vector<int>& piles,int mid,int h){
+        int count=0;
+        for(int i=0;i<piles.size();i++){
+            int element=piles[i];
+            count+=element/mid+1;
+            if(element%mid==0) count--;
+            if(count>h) return false;
+        }
+        return true;
+       }
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n=piles.size();
+        int s=1,e= *max_element(piles.begin(), piles.end());
+        int ans=e;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(solve(piles,mid,h)){
+                ans=mid;
+                e=mid-1;
+            }
+            else{
+                s=mid+1;
+            }
+        }
+        return ans;
+    }
+};
