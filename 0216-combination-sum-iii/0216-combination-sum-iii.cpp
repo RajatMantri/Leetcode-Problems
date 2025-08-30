9502689b2319c87896307a5fb6a7f5bc26a6841a
Rajat Mantri <115399183+RajatMantri@users.noreply.github.com>
2025-08-30 14:19:30 +0530

    Time: 0 ms (100%), Space: 9 MB (29.96%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void f(int rem,int target,vector<vector<int>> &ans,vector<int> &temp,int num){
+        if(num>=10){
+            if(rem==0&&target==0) ans.push_back(temp);
+            return;
+        }
+
+        f(rem,target,ans,temp,num+1);
+
+        temp.push_back(num);
+        f(rem-1,target-num,ans,temp,num+1);
+        temp.pop_back();
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        f(k,n,ans,temp,1);
+        return ans;
+    }
+};
