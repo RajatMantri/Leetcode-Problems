7caa62a3830ebf4506de799065fa5003b4b7c755
Rajat Mantri <115399183+RajatMantri@users.noreply.github.com>
2025-09-02 17:09:44 +0530

    Time: 18 ms (17.75%), Space: 12.6 MB (48.23%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -15,7 +15,7 @@ public:
         int maxi=1,last=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<i;j++){
-                if(nums[i]%nums[j]==0&&dp[i]<dp[j]+1){
+                if((nums[i]%nums[j]==0||(nums[j]%nums[i]==0))&&dp[i]<dp[j]+1){
                     dp[i]=dp[j]+1;
                     parent[i]=j;
                 }
